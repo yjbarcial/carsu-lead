311525dbb66838607306ea6ef723554381312eca
yjbarcial <yjbarcial@gmail.com>
2026-06-03 09:28:02 +0800

    removed GoogleStrategy

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -9,7 +9,6 @@ import { RefreshToken } from './refresh-token.entity';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategies/jwt.strategy';
-import { GoogleStrategy } from './strategies/google.strategy';
 
 @Module({
   imports: [
@@ -25,7 +24,7 @@ import { GoogleStrategy } from './strategies/google.strategy';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, GoogleStrategy],
+  providers: [AuthService, JwtStrategy],
   exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
